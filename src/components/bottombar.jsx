b5bccdc96f33b4171c0c54a1a99b3d7aadedb97f
A Ram Pandey <shashwatpandey100@gmail.com>
2025-02-16 21:28:10 +0530

    before posting on linkedin

--- src/components/bottombar.jsx
+++ src/components/bottombar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Bottombar = () => {
   return (
-    <div className='w-full h-[25px] bg-[#10182d]'></div>
+    <div className='w-full h-[25px] bg-[#10182d] flex justify-between items-center px-5'>
+      <span></span>
+      <span className='text-[11px]'>Made with 💜 by <a target="_blank" href="https://www.linkedin.com/in/shashwat-pandey-544bb6254/" className='underline ml-1'>Shashwat</a></span>
+    </div>
   )
 }
 
